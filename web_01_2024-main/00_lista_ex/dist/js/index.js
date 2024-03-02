ce1259e46d879692f5105becd9f022a5ae09632b
Brunno-Bocardo <brunno.pb@outlook.com>
2024-03-01 22:34:29 -0300

    continuação

--- web_01_2024-main/00_lista_ex/dist/js/index.js
+++ web_01_2024-main/00_lista_ex/dist/js/index.js
@@ -33,4 +33,55 @@ function mediaArray(...numeros) {
     }
     return media / tamanho;
 }
-console.log(mediaArray(1, 1, 1));
+// console.log(mediaArray(1,1,1))
+// FUNÇÃO 4
+function convertToUpper(text) {
+    let new_text = text.toUpperCase();
+    return new_text;
+}
+// console.log(convertToUpper("ola mundo"))
+// console.log(convertToUpper("meu precioso"))
+// FUNÇÃO 5
+function primoOrNot(val) {
+    let i;
+    if (val == 2 || 1) {
+        return true;
+    }
+    else {
+        for (i = 2; i < (val); i++) {
+            if (val % i == 0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+}
+// console.log(primoOrNot(1))
+// FUNÇÃO 6
+function inverterArray(...array) {
+    let tamanho = array.length;
+    let new_array = [];
+    let i;
+    for (i = 0; i < tamanho; i++) {
+        let aux = array.pop();
+        new_array.push(aux);
+    }
+    return new_array;
+}
+// console.log(inverterArray(1,2,3,4,97))
+// FUNÇÃO 7
+function porcentagem(valor, porcentagem) {
+    return valor * ((porcentagem / 100) + 1);
+}
+// console.log(porcentagem(2500, 10))
+// FUNÇÃO 8
+function reverterFrase(frase) {
+    let fraseArray = frase.split(" ");
+    return fraseArray.length;
+}
+// console.log(reverterFrase("ola pessoas")) 
+let string = 'oi pessoal';
+let palavra = string.split(" ");
+console.log(palavra);
--- web_01_2024-main/00_lista_ex/src/index.ts
+++ web_01_2024-main/00_lista_ex/src/index.ts
@@ -35,4 +35,69 @@ function mediaArray(...numeros:number[]):number {
     }
     return media/tamanho
 }
-console.log(mediaArray(1,1,1))
+// console.log(mediaArray(1,1,1))
+
+
+// FUNÇÃO 4
+function convertToUpper(text:string) : string {
+    let new_text:string = text.toUpperCase()
+    return new_text
+}
+// console.log(convertToUpper("ola mundo"))
+// console.log(convertToUpper("meu precioso"))
+
+
+// FUNÇÃO 5
+function primoOrNot(val:number){
+    let i:number 
+
+    if (val== 2 || 1) {
+        return true
+    }
+    else {
+        for (i=2; i<(val); i++) {
+            if (val%i == 0) {
+                return false
+            }
+            else {
+                return true
+            }
+        }
+    }
+}
+// console.log(primoOrNot(1))
+
+
+// FUNÇÃO 6
+function inverterArray(...array:number[]) {
+    let tamanho:number = array.length
+    let new_array = []
+    let i:number
+
+    for (i=0; i<tamanho; i++) {
+        let aux = array.pop()
+        new_array.push(aux)
+    }
+    
+    return new_array
+}
+// console.log(inverterArray(1,2,3,4,97))
+
+
+// FUNÇÃO 7
+function porcentagem(valor:number, porcentagem:number) {
+    return valor * ((porcentagem/100) + 1)
+}
+// console.log(porcentagem(2500, 10))
+
+
+// FUNÇÃO 8
+function reverterFrase(frase:string) {
+    let fraseArray:string[] = frase.split(" ")
+    return fraseArray.length
+}
+// console.log(reverterFrase("ola pessoas")) 
+
+let string:string = 'oi pessoal'
+let palavra:string[] = string.split(" ")
+console.log(palavra)

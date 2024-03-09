442e797220bc495adcd0ab5989e50d31f301e6af
Brunno-Bocardo <brunno.pb@outlook.com>
2024-03-08 22:29:25 -0300

    lista 2

--- web_01_2024-main/Lista_2_class/dist/js/index.js
+++ web_01_2024-main/Lista_2_class/dist/js/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// EXERCÍCIO 1 
 class Carro {
     constructor(cor, portas, ganhaDoFusca, ano) {
         this.cor = cor;
@@ -30,10 +31,98 @@ class Carro {
     set setAno(ano) {
         this.ano = ano;
     }
-    idadeCarro(idadeDoCarro) {
+    idadeCarro() {
         let dataAtual = new Date().getFullYear();
-        let result = dataAtual - idadeDoCarro;
+        let result = dataAtual - this.ano;
+        return result;
     }
 }
-let dataAtual = new Date();
-console.log(dataAtual);
+const carrinho = new Carro("vermelho", 3, true, 1900);
+// console.log(carrinho.idadeCarro())
+// console.log(carrinho.getGanhaDoFusca)
+// console.log(carrinho.getCor)
+// console.log(carrinho.setPortas=2)
+// EXERCÍCIO 2 
+class Calculadora {
+    constructor(valor1, valor2) {
+        this.val1 = valor1;
+        this.val2 = valor2;
+    }
+    soma() {
+        return this.val1 + this.val2;
+    }
+    subtracao() {
+        return this.val1 - this.val2;
+    }
+    multiplicacao() {
+        return this.val1 * this.val2;
+    }
+    divisao() {
+        if (this.val2 == 0)
+            return "NÃO DIVIDE POR ZERO PÔ  >:(";
+        else
+            return this.val1 / this.val2;
+    }
+    porcentagem() {
+        let aux = this.val2 / 100;
+        return aux * this.val1;
+    }
+    get getVal1() {
+        return this.val1;
+    }
+    get getVal2() {
+        return this.val2;
+    }
+    set setVal1(val1) {
+        this.val1 = val1;
+    }
+    set setVal2(val2) {
+        this.val2 = val2;
+    }
+}
+// const conta = new Calculadora(15, 100)
+// console.log(conta.soma())
+// console.log(conta.subtracao())
+// console.log(conta.multiplicacao())
+// console.log(conta.divisao())
+// console.log(conta.porcentagem())
+// console.log(conta.getVal1)
+// console.log(conta.getVal2)
+// console.log(conta.setVal2=0)
+// console.log(conta.divisao())
+class Produto {
+    constructor(nome, preco, quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+    calcularValorTotalEmEstoque() {
+        return this.preco * this.quantidadeEmEstoque;
+    }
+    reporEstoque(quantidade) {
+        return this.quantidadeEmEstoque += quantidade;
+    }
+    vender(quantidade) {
+        let quantidadeAtual = this.quantidadeEmEstoque;
+        if (quantidade > quantidadeAtual)
+            console.log("Infelizmente, nosso estoque só tem: ", quantidade, ". Acontece");
+        else
+            return this.quantidadeEmEstoque -= quantidade;
+        console.log("Compra feita  :)");
+    }
+    get getNome() {
+        return this.nome;
+    }
+    get getPreco() {
+        return this.preco;
+    }
+    get getQuantidadeEmEstoque() {
+        return this.quantidadeEmEstoque;
+    }
+}
+const bananas = new Produto("banana", 10, 200);
+console.log(bananas.calcularValorTotalEmEstoque());
+console.log(bananas.reporEstoque(300));
+console.log(bananas.getQuantidadeEmEstoque);
+console.log(bananas.vender(150));
+console.log(bananas.getQuantidadeEmEstoque);
--- web_01_2024-main/Lista_2_class/src/index.ts
+++ web_01_2024-main/Lista_2_class/src/index.ts
@@ -1,3 +1,4 @@
+// EXERCÍCIO 1 
 class Carro {
     private cor:string
     private portas:number
@@ -43,12 +44,129 @@ class Carro {
         this.ano = ano
     }
 
-    idadeCarro(idadeDoCarro:number) {
+    idadeCarro() {
         let dataAtual = new Date().getFullYear()
-        let result = dataAtual - idadeDoCarro
+        let result = dataAtual - this.ano
+        return result
     }
 }
 
+const carrinho = new Carro("vermelho",3,true,1900)
+// console.log(carrinho.idadeCarro())
+// console.log(carrinho.getGanhaDoFusca)
+// console.log(carrinho.getCor)
+// console.log(carrinho.setPortas=2)
 
-let dataAtual = new Date()
-console.log(dataAtual)
+
+
+// EXERCÍCIO 2 
+class Calculadora {
+    private val1:number
+    private val2:number
+
+    constructor (valor1: number, valor2:number) {
+        this.val1 = valor1
+        this.val2 = valor2
+    }
+
+    soma() {
+        return this.val1 + this.val2
+    }
+
+    subtracao() {
+        return this.val1 - this.val2
+    }
+
+    multiplicacao() {
+        return this.val1 * this.val2
+    }
+
+    divisao() {
+        if (this.val2 == 0)
+            return "NÃO DIVIDE POR ZERO PÔ  >:("
+        else
+            return this.val1 / this.val2
+    }
+
+    porcentagem() {
+        let aux = this.val2/100
+        return aux*this.val1
+    }
+
+    get getVal1() {
+        return this.val1
+    }
+
+    get getVal2() {
+        return this.val2
+    }
+
+    set setVal1(val1:number) {
+        this.val1 = val1
+    }
+
+    set setVal2(val2:number) {
+        this.val2 = val2
+    }
+}
+
+// const conta = new Calculadora(15, 100)
+// console.log(conta.soma())
+// console.log(conta.subtracao())
+// console.log(conta.multiplicacao())
+// console.log(conta.divisao())
+// console.log(conta.porcentagem())
+// console.log(conta.getVal1)
+// console.log(conta.getVal2)
+// console.log(conta.setVal2=0)
+// console.log(conta.divisao())
+
+
+
+class Produto {
+    private nome:string
+    private preco:number
+    private quantidadeEmEstoque:number
+
+    constructor (nome:string, preco:number, quantidadeEmEstoque:number) {
+        this.nome = nome
+        this.preco = preco
+        this.quantidadeEmEstoque = quantidadeEmEstoque
+    }
+
+    calcularValorTotalEmEstoque() {
+        return this.preco * this.quantidadeEmEstoque
+    }
+
+    reporEstoque(quantidade:number) {
+        return this.quantidadeEmEstoque += quantidade
+    }
+
+    vender(quantidade:number) {
+        let quantidadeAtual = this.quantidadeEmEstoque
+        if (quantidade > quantidadeAtual)
+            console.log("Infelizmente, nosso estoque só tem: ", quantidade, ". Acontece")
+        else
+            return this.quantidadeEmEstoque -= quantidade
+            console.log("Compra feita  :)")
+    }
+
+    get getNome() {
+        return this.nome
+    }
+
+    get getPreco() {
+        return this.preco
+    }
+
+    get getQuantidadeEmEstoque() {
+        return this.quantidadeEmEstoque
+    }
+}
+
+const bananas = new Produto("banana", 10, 200)
+console.log(bananas.calcularValorTotalEmEstoque())
+console.log(bananas.reporEstoque(300))
+console.log(bananas.getQuantidadeEmEstoque)
+console.log(bananas.vender(150))
+console.log(bananas.getQuantidadeEmEstoque)

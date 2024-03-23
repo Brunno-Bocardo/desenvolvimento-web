574fe6dd0a857f87deac187aa10ae7d68287b849
Brunno-Bocardo <brunno.pb@outlook.com>
2024-03-22 22:28:13 -0300

    teste

--- web_01_2024-main/Aula_6_API/dist/app.js
+++ web_01_2024-main/Aula_6_API/dist/app.js
@@ -6,20 +6,21 @@ var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
 function appLog() {
     console.log("A API está disponível na porta http://localhost:3000");
 }
-function hello(resq, res) {
+// req : o que eu quero pegar pra usar aqui dentro
+// res : o que eu quero retornar 
+function hello(req, res) {
+    const obj = req.body;
+    console.log(obj);
     return res.status(201).json({
-        mensagem: "Hello User, my name is machine",
-        mensagem2: "Prometo que seu fim será breve"
+        mensagem: "Produto adicionado com sucesso!",
+        objeto: obj
     });
 }
 ;
-// function name(resq:Request, res:Response, value:string) {
-//     return res.send("Hello User, my name is machine") 
-// }
 app.get("/api/hello", hello);
-// app.post("/api/name", )
 app.listen(PORT, appLog);
--- web_01_2024-main/Aula_6_API/src/app.ts
+++ web_01_2024-main/Aula_6_API/src/app.ts
@@ -1,25 +1,31 @@
 import express, {Request, Response} from 'express';
 
 const app = express();
+app.use(express.json())
 const PORT = process.env.PORT ?? 3000;
 
 function appLog(){
     console.log("A API está disponível na porta http://localhost:3000")
 }
 
-function hello(resq:Request, res:Response) {
+// req : o que eu quero pegar pra usar aqui dentro
+// res : o que eu quero retornar 
+function hello(req:Request, res:Response) {
+    const obj: {
+        name: string,
+        description: string,
+        price: number
+    } = req.body;
+    console.log(obj)
+
     return res.status(201).json({
-        mensagem: "Hello User, my name is machine",
-        mensagem2: "Prometo que seu fim será breve"
+        mensagem: "Produto adicionado com sucesso!",
+        objeto:obj
     });
 };
 
-// function name(resq:Request, res:Response, value:string) {
-//     return res.send("Hello User, my name is machine") 
-// }
 
 app.get("/api/hello", hello)
-// app.post("/api/name", )
 
 
 app.listen(PORT, appLog)

9e2558bf724167e60bad14fa52b2491aacdc66e3
Brunno-Bocardo <brunno.pb@outlook.com>
2024-08-19 21:42:16 -0300

    models

--- web_01_2024-main/Interdisciplinar/src/model/Ficha.ts
+++ web_01_2024-main/Interdisciplinar/src/model/Ficha.ts
@@ -1,23 +1,26 @@
-import { DateTime } from "luxon";  // Usando a biblioteca luxon para manipulação de datas
-
 export class Ficha {
     private static instance: Ficha | null = null;
     id: number;
     petID: number;
     relatorio: string;
-    data_hora: DateTime;
+    data_hora: Date;
 
-    private constructor(id: number, petID: number, relatorio: string, data_hora: DateTime) {
+    private constructor(id: number, petID: number, relatorio: string, data_hora: Date) {
         this.validatesInformation(petID, relatorio);
         this.id = id;
         this.petID = petID;
         this.relatorio = relatorio;
-        this.data_hora = data_hora;  // Inicializando data_hora
+        this.data_hora = data_hora; 
     }
 
-    public static getInstance(id: number, petID: number, relatorio: string, data_hora: DateTime): Ficha {
+    public static getInstance(id: number, petID: number, relatorio: string, data_hora: Date): Ficha {
         if (!Ficha.instance) {
             Ficha.instance = new Ficha(id, petID, relatorio, data_hora);
+        } else {
+            Ficha.instance.id = id || Ficha.instance.id;
+            Ficha.instance.petID = petID || Ficha.instance.petID;
+            Ficha.instance.relatorio = relatorio || Ficha.instance.relatorio;
+            Ficha.instance.data_hora = data_hora || Ficha.instance.data_hora;
         }
         return Ficha.instance;
     }
